800303ee003f483017e82d3c6f426aea780e7701
JoaoCostaTI <joao.costapp@outlook.com>
2025-07-17 16:27:14 -0300

    Update arena.py

--- arena.py
+++ arena.py
@@ -1,5 +1,7 @@
-from time import sleep
-
-print('Algo')
-sleep(0.3)
-print('Algo')
+try:
+    # Código que pode gerar um erro
+    numero = int(input("Digite um número: "))
+    print(f"Você digitou {numero}")
+except:
+    # Código executado se ocorrer qualquer erro
+    print("Ocorreu um erro!")

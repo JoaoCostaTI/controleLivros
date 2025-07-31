3afe2d27c0a9e714b6f092c4b38b78b942988008
JoaoCostaTI <joao.costapp@outlook.com>
2025-07-31 11:51:16 -0300

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,26 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 # Função para abrir nova janela
 def abrir_janela_opcao(opcao):
     nova_janela = tk.Toplevel(janela)  # Cria nova janela filha
     nova_janela.title(f"Opção: {opcao}")
     nova_janela.geometry("300x200")  # Tamanho da janela
     
-    tk.Label(nova_janela, text=f"Você escolheu a opção: {opcao}").pack(pady=20)
+    tk.Label(nova_janela, text=f"{opcao}").pack(pady=20)
     
     # Exemplo de botão na nova janela
     tk.Button(nova_janela, text="Fechar", command=nova_janela.destroy).pack(pady=10)
 
 # Janela principal
 janela = tk.Tk()
-janela.title("Menu Principal")
-janela.geometry("250x200")
+janela.title("SKOOB")
+janela.geometry("300x250")
 
 # Lista de opções
-opcoes = ["Cadastrar Alimento", "Editar Alimento", "Excluir Alimento"]
+opcoes = ["Cadastrar Livro", "Listar Livro", "Estatisticas", "Editar Livro", "Sair do Programa"]
 
-tk.Label(janela, text="Escolha uma opção:").pack(pady=10)
+tk.Label(janela, text="Sistema de Controle de Livros").pack(pady=10)
 
 for opcao in opcoes:
     tk.Button(janela, text=opcao, width=25,
